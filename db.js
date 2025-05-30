0c1367ff501dab0e7b85f7a305d59fd35962e992
Maysa Araujo <maysaaraujo25@gmail.com>
2025-05-29 22:11:34 -0300

    API Node com Postgres

---
+++ db.js
@@ -0,0 +1,8 @@
+// db.js
+import dotenv from 'dotenv';
+import http from 'http';
+import { neon } from '@neondatabase/serverless';
+
+dotenv.config();
+
+export const sql = neon(process.env.DATABASE_URL);
